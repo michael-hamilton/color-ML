24180baa654f75fd33290d45122a3632ed7edde0
Michael Hamilton <mike@miska.me>
2023-01-07 20:10:11 -0500

    Added extensions to files

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import ColorML from './color-ml'
+import ColorML from './color-ml.js'
 
 const root = createRoot(document.getElementById('root'));
 
